18c64f496166035415be34dfbd23331cf1cc71da
Stepan <bai_777@mail.ru>
2022-08-07 15:56:16 +0300

    создал MainFragment.kt, BaseMainFragment.kt

--- app/src/main/java/com/example/geoloacation/MainActivity.kt
+++ app/src/main/java/com/example/geoloacation/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.example.geoloacation
 
+import android.Manifest
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
+import com.example.geoloacation.databinding.ActivityMainBinding
+import com.yandex.mapkit.MapKitFactory
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val locationPermissionRequest = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            when {
+                permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                    // Precise location access granted.
+                }
+                permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                    // Only approximate location access granted.
+                }
+                else -> {
+                    // No location access granted.
+                }
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        initYandexMapKits()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        locationPermissionRequest.launch(arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION))
+        initFragment()
+    }
+
+
+    private fun initYandexMapKits() {
+        MapKitFactory.setApiKey(BuildConfig.YAPI_KEY)
+        MapKitFactory.initialize(this)
+    }
+
+    private fun initFragment(fragment: Fragment = MainFragment()) {
+        supportFragmentManager.beginTransaction()
+            .replace(binding.container.id, fragment)
+            .commitNow()
+    }
+
+    fun getContainer(): Int {
+        return binding.container.id
     }
 }
